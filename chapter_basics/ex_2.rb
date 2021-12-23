90a5238eb93a2b1072dc6646179cb9b42ee692b0
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-12-22 21:18:19 -0500

    two exes

---
+++ chapter_basics/ex_2.rb
@@ -0,0 +1,8 @@
+thousands = 1234/1000
+puts thousands
+hundreds = 1234 % 1000 / 100
+puts hundreds
+tens = 1234 % 100 / 10
+puts tens
+ones = 1234 % 10 
+puts ones 
---
+++ chapter_basics/ex_3.rb
@@ -0,0 +1,9 @@
+movies = {
+  sleepless_in_seattle: 1993,
+  the_revanant: 2015,
+  the_incredibles: 2004
+}
+# # movies.each{|k,v| puts v}
+puts movies[:sleepless_in_seattle]
+puts movies[:the_revanant]
+puts movies[:the_incredibles]
