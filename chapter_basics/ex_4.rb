9fb456b42a30c83c8286ee2615f2ec8736a65c11
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-12-22 21:24:37 -0500

    add two more exes

---
+++ chapter_basics/ex_4.rb
@@ -0,0 +1,4 @@
+movie_years = [1993,2016,2004]
+puts movie_years[0]
+puts movie_years[1]
+puts movie_years[2]
---
+++ chapter_basics/ex_5.rb
@@ -0,0 +1,4 @@
+puts factorial_5 = 1*2*3*4*5
+puts factorial_6 = 1*2*3*4*5*6
+puts factorial_7 = 1*2*3*4*5*6*7
+puts factorial_8 = 1*2*3*4*5*7*8
