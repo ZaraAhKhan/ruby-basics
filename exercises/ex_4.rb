a37520bdf600a18668814e26215eedaefbad2291
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-12-25 01:11:21 -0500

    add practice exercises

---
+++ exercises/ex_4.rb
@@ -0,0 +1,12 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr.push(11).unshift(0)
+puts arr
+
+#ex_5
+arr.pop
+arr.push(3)
+puts arr
+
+#ex_6
+arr.uniq!
+puts arr
---
+++ exercises/ex_8.rb
@@ -0,0 +1,2 @@
+hash_one = {name:"Belle"}
+hash_two = {:name => "Belle"}
